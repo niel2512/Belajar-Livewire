c1c7d6dd6a73936d53559475bb56667bd1f02df3
niel2512 <nielyusuf17@gmail.com>
2025-11-17 13:59:21 +0700

    Belajar Components & Properties pada Livewire

---
+++ app/Livewire/Users.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace App\Livewire;
+
+use App\Models\User;
+use Livewire\Component;
+
+class Users extends Component
+{
+    // public $title = 'Users Page';
+    public function render()
+    {
+        return view('livewire.users', [
+            'title' => 'Users Page',
+            'users' => User::all(),
+        ]);
+    }
+}
---
+++ resources/views/livewire/users.blade.php
@@ -0,0 +1,4 @@
+<div>
+    <h1 class="text-3xl font-bold">{{ $title }}</h1> {{-- diambil dari Users.php di function render --}}
+    <p>Users Count : {{ count($users) }}</p> {{-- diambil dari Users.php di function render --}}
+</div>
