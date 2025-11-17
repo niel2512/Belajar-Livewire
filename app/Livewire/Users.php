f228c38a3cae9fe21244ae04590d87e83f8a4d73
niel2512 <nielyusuf17@gmail.com>
2025-11-17 14:13:50 +0700

    Bikin function createUser

--- app/Livewire/Users.php
+++ app/Livewire/Users.php
@@ -3,10 +3,20 @@
 namespace App\Livewire;
 
 use App\Models\User;
+use Illuminate\Support\Facades\Hash;
 use Livewire\Component;
 
 class Users extends Component
 {
+    public function createUser()
+    {
+        // dd('button click');
+        User::create([
+            'name' => 'Nathaniel Yusuf Langelo',
+            'email' => 'niel@gmail.com',
+            'password' => Hash::make('password'),
+        ]);
+    }
     // public $title = 'Users Page';
     public function render()
     {
