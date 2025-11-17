b5044fec4b2cf2b2869a7e244da5bdfb04d6974a
niel2512 <nielyusuf17@gmail.com>
2025-11-17 15:20:04 +0700

    Bikin validasi menggunakan Anotation PHP & nambahin session untuk flash message

--- app/Livewire/Users.php
+++ app/Livewire/Users.php
@@ -4,17 +4,39 @@ namespace App\Livewire;
 
 use App\Models\User;
 use Illuminate\Support\Facades\Hash;
+use Livewire\Attributes\Validate;
 use Livewire\Component;
 
 class Users extends Component
 {
+    // Public property
+    // Pakai anotation
+    #[Validate('required|min:3')]
     public $name = '';
+    #[Validate('required|email:dns|unique:users')]
     public $email = '';
+    #[Validate('required|min:8')]
     public $password = '';
 
     public function createUser()
     {
+
+        // $validated = $this->validate([
+        //     'name' => 'required|min:3',
+        //     'email' => 'required|email:dns|unique:users', //dns agar membaca .com .org dll, unique agar email harus beda
+        //     'password' => 'required|min:8'
+        // ]);
+
         // dd('button click');
+
+        // User::create([
+        //     'name' => $validated['name'],
+        //     'email' => $validated['email'],
+        //     'password' => Hash::make($validated['password']),
+        // ]);
+
+        $this->validate();
+
         User::create([
             'name' => $this->name,
             'email' => $this->email,
@@ -22,6 +44,7 @@ class Users extends Component
         ]);
 
         $this->reset();
+        session()->flash('sukses', 'New user has been created');
     }
     // public $title = 'Users Page';
     public function render()
