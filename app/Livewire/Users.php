2bb427b0b00d428465d73ea3fb8f19e922ff6892
niel2512 <nielyusuf17@gmail.com>
2025-11-18 16:26:28 +0700

    Nambahin pagination livewire

--- app/Livewire/Users.php
+++ app/Livewire/Users.php
@@ -7,10 +7,11 @@ use Illuminate\Support\Facades\Hash;
 use Livewire\Attributes\Validate;
 use Livewire\Component;
 use Livewire\WithFileUploads;
+use Livewire\WithPagination;
 
 class Users extends Component
 {
-    use WithFileUploads;
+    use WithFileUploads, WithPagination;
     // Public property
     // Pakai anotation
     #[Validate('required|min:3')]
@@ -46,7 +47,7 @@ class Users extends Component
     {
         return view('livewire.users', [
             'title' => 'Users Page',
-            'users' => User::all(),
+            'users' => User::latest()->paginate(5),
         ]);
     }
 }
